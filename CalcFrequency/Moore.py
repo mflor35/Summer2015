b977d789e473cbe72fa9ca00ccf63f8e86aec746
Miguel Flores Silverio <floresmigu3l@gmail.com>
2015-07-09 14:25:40 -0700

    Sample voltage values

--- CalcFrequency/Moore.py
+++ CalcFrequency/Moore.py
@@ -9,10 +9,10 @@ delta_time = 1.0 / sample_frequency
 nominal_frequency = 60 #Nominal Frequency
 testing_frequency = 59
 max_voltage = 120 #Amplitude of the test signal
-number_of_samples = 5000
+number_of_samples = 10000
 x = []
 for t in frange(delta_time,number_of_samples * delta_time,delta_time):
     x.append(max_voltage * math.sin(2 * math.pi * nominal_frequency * t))
 
-for v in x:
-    print v
+for voltage in x:
+    print voltage

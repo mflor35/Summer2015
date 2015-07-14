fbe7f6ad670ef31bdd009fc785886760250bc482
Miguel Flores Silverio <floresmigu3l@gmail.com>
2015-07-14 15:34:28 -0700

    Graphing tweet-a-watt data and configuration for the reciever

--- WirelessCommunication/radios_with_file_output.py
+++ WirelessCommunication/radios_with_file_output.py
@@ -12,7 +12,7 @@ the transmitter file will transmit the voltage, followed by the frequency
 
 from xbee import XBee
 import serial
-from time import sleep
+from time import strftime
 tweetawatt = open('tweetawatt_data.txt', 'w')
 #Change serial port to COM1,COM2... when running this script on a Windows machine.
 ser = serial.Serial('/dev/ttyUSB0', 9600)
@@ -20,8 +20,9 @@ xbee = XBee(ser)
 i = 0 #counter for the response (assuming odd is voltage and even is frequency)
 while True:
     try:
-        sleep(2)
         response = xbee.wait_read_frame()
+        response = dict(response)
+        response["datestamp"] = strftime("%Y-%m-%d %H:%M:%S")
         tweetawatt.write(str(response)+"\n")
     except KeyboardInterrupt:
         break

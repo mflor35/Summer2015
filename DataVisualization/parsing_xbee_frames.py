8228fbbca35fda57834283d96b4d7d5d0a93cf57
Kapil Sinha <saradasinha@yahoo.com>
2015-07-14 16:34:38 -0700

    I did not change the output or make huge changes here but made it more streamlined. Instead of having a for statement with an if-else statement, I just replaced it with a .split() statement.

--- DataVisualization/parsing_xbee_frames.py
+++ DataVisualization/parsing_xbee_frames.py
@@ -1,15 +1,9 @@
 import json
-readings = file("original_frequency_output.txt")
-listReadings = []
-for line in readings:
-    tempreading = ""
-    for char in line:
-        if char != '}':
-            tempreading += char
-        else:
-            tempreading += '}'
-            listReadings.append(tempreading)
-            tempreading = ""
-
+readings = open("original_frequency_output.txt", 'r')
+stringReadings = readings.read()
+print type(stringReadings)
+listReadings = stringReadings.split('}')
+print len(listReadings)
 for reading in listReadings:
     reading = json.loads(reading)
+print reading

3eceaa9e50d93fe68e9ca4ab55cab9147604d66f
Miguel Flores Silverio <floresmigu3l@gmail.com>
2015-07-17 15:29:49 -0700

    Quick scripts [Not necessary]

--- DataAnalysis/parsing_xbee_frames.py
+++
@@ -1,9 +0,0 @@
-import json
-readings = open("original_frequency_output.txt", 'r')
-stringReadings = readings.read()
-print type(stringReadings)
-listReadings = stringReadings.split('}')
-print len(listReadings)
-for reading in listReadings:
-    reading = json.loads(reading)
-print reading

4bc56a6e53e354f3f11db33684905c40d02a4519
Kapil Sinha <saradasinha@yahoo.com>
2015-07-22 10:43:24 -0700

    Volatage and Current data is normalized properly. Better graphing added second y-axis

--- DataAnalysis/graph_data.py
+++ DataAnalysis/graph_data.py
@@ -1,5 +1,5 @@
 import ast
-from pylab import *
+import matplotlib.pyplot as plt
 from time import sleep
 """
 normalizeData takes in analog readings of the  current(adc-4) and the volatage(adc-0).
@@ -9,16 +9,16 @@ It also takes in the sensorAddr(source_addr) simply to keep track where is the d
 @current:  a list of 19 analog readings [492, 492, 510, 491, 492, 491, 491, 491, 492, 480, 492, 492, 492, 492, 492, 492, 497, 492]
 
 """
-def normalizeData(voltage,current,sensorAddr):
+def normalizeData(voltage,current):
     # Normalize the curve to zero
     # From and more at Adafruit design https://learn.adafruit.com/tweet-a-watt/design-listen
 
-    MAINSVPP = 170 * 2 # +-170V is what 120Vrms ends up being
+    MAINSVPP = 164 * 2 # +-164V
     VREF = 498         # Hardcoded 'DC bias' value its about 492
     CURRENTNORM = 15.5 # Normalizing constant that converts the analog reading to Amperes
 
-    max_v = 1024       # XBee ADC is 10 bits, so max value is 1023
-    min_v = 0
+    min_v = 1024       # XBee ADC is 10 bits, so max value is 1023
+    max_v = 0
 
     # Find the smallest voltage and the biggest voltage in the list of samples taken
     for v in voltage:
@@ -42,30 +42,34 @@ def normalizeData(voltage,current,sensorAddr):
     for index in range(len(current)):
         current[index] -= VREF
         current[index] /= CURRENTNORM
-    return sensorAddr,voltage,current
+    return voltage,current
 def main():
     #filepath = raw_input("Enter name of data file or path where it is located: ")
-    data = open("WirelessCommunication/data/rawdata.txt")
-    ion()
+    data = open("data_sample/rawdata1.txt")
+    plt.ion()
+    fig,ax1 = plt.subplots()
+    ax2 = plt.twinx()
     for reading in data:
         reading = reading.strip()
         adc0 = []
         adc4 = []
-        cla()
+        ax1.cla()
+        ax2.cla()
         try:
             reading = ast.literal_eval(reading)
-            sensorAddr = reading['sorce_addr']
+            sensorAddr = reading['source_addr']
             for sample in reading['samples']:
                 adc0.append(sample['adc-0'])
                 adc4.append(sample['adc-4'])
             voltage,current = normalizeData(adc0,adc4)
-            subplot(111)
-            plot(current,'r')
-            xlabel("Sample #")
-            ylabel("Voltage")
-            ylim(-200,200)
-            draw()
-            sleep(.3)
+            print voltage
+            ax1.plot(voltage,'r')
+            ax1.set_xlabel("Sample #")
+            ax1.set_ylabel("Voltage")
+            ax2.plot(current,'b')
+            ax2.set_ylabel("Current")
+            sleep(1)
+            plt.draw()
         except KeyboardInterrupt:
             break
 main()
